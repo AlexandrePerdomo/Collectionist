b3cbe786ddf7cf673e099665525662376dab6284
Alexandre Perdomo <aperdomo@free.fr>
2021-03-17 11:08:50 +0100

    adding i18n locales and change Timezone

--- config/application.rb
+++ config/application.rb
@@ -25,13 +25,15 @@ module Collectiotionsit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.available_locales = [:en, :fr]
+    config.i18n.default_locale = :fr
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Europe/Paris"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
