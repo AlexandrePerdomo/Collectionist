3187984af216d7ead47c44287ab02ed218ea18a3
Alexandre Perdomo <aperdomo@free.fr>
2021-03-18 13:20:17 +0100

    modification on generate_open_hours method to avoid shift in views

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,11 +3,15 @@
 class Shop < ApplicationRecord
   validates :name, presence: true
   has_many :open_hours, dependent: :destroy
+  accepts_nested_attributes_for :open_hours, allow_destroy: true, reject_if: :all_blank
 
   def generate_open_hours
+    # Add closing days
     open_hours.append(add_missing_days)
-    orderred_days = open_hours.sort_by(&:day).rotate(Time.zone.today.strftime('%u').to_i)
-    orderred_days.group_by(&:day)
+    # Order open_hours by day in a hash
+    orderred_days = open_hours.sort_by(&:day).group_by(&:day)
+    # Convert hash to array, rotate it then convert back to hash
+    orderred_days.to_a.rotate(Time.zone.today.strftime('%u').to_i - 1).to_h
   end
 
   def add_missing_days
