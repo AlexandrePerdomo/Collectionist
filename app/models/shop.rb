57edadd79858c84a79ecbaf063eb756173d3c2f8
Alexandre Perdomo <aperdomo@free.fr>
2021-03-18 10:09:36 +0100

    new method and tests to generate open_hours hash for a shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,18 @@
 class Shop < ApplicationRecord
   validates :name, presence: true
   has_many :open_hours, dependent: :destroy
+
+  def generate_open_hours
+    open_hours.append(add_missing_days)
+    orderred_days = open_hours.sort_by(&:day).rotate(Time.zone.today.strftime('%u').to_i)
+    orderred_days.group_by(&:day)
+  end
+
+  def add_missing_days
+    missing_days = []
+    ((1..7).to_a - open_hours.map(&:day)).each do |missing_day|
+      missing_days << OpenHour.new(day: missing_day, closed: true)
+    end
+    missing_days
+  end
 end
--- test/models/shop_test.rb
+++ test/models/shop_test.rb
@@ -5,4 +5,19 @@ require 'test_helper'
 class ShopTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
   should have_many(:open_hours)
+
+  test 'add_missing_days should generate missing days' do
+    missing_days = shops(:one).add_missing_days
+    assert_equal missing_days.length, 5
+    assert_equal missing_days.map(&:day), [2, 4, 5, 6, 7]
+  end
+
+  test 'generate_open_hours should generate open_hours hash' do
+    Timecop.freeze(today_for_test) # Vendredi
+    open_hours_hash = shops(:one).generate_open_hours
+    assert_equal open_hours_hash.keys, [5, 6, 7, 1, 2, 3, 4]
+    assert_equal open_hours_hash[1].length, 2
+    assert_equal open_hours_hash[3].length, 1
+    assert_equal open_hours_hash[7].length, 1
+  end
 end
