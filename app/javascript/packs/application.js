7fa41df85f46cc6ef2d0032f3a378dc2849301a8
Alexandre Perdomo <aperdomo@free.fr>
2021-03-18 16:19:41 +0100

    configuration bootstrap + simpleform

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,12 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import "bootstrap"
+
 require("jquery")
 require("@nathanvda/cocoon")
+
+
 
 Rails.start()
 Turbolinks.start()
